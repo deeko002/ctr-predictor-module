3ff45a620131f3454d56f3287e681b7950ea4556
Tarun Kumar Deekonda <deeko002@umn.edu>
2025-04-19 22:41:53 -0500

    added timestamp in logger

--- logger.py
+++ logger.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import os
+from datetime import datetime
 
 LOG_PATH = "prediction_log.csv"
 
@@ -10,13 +11,26 @@ def load_log():
         return pd.read_csv(LOG_PATH)
     else:
         return pd.DataFrame(columns=[
-            "hour", "banner_pos", "device_type", "device_model", "predicted_ctr"
+            "timestamp", "hour", "banner_pos", "device_type", "device_model", "predicted_ctr"
         ])
 
 def log_prediction(input_dict, predicted_ctr):
     df = load_log()
+
     new_row = input_dict.copy()
     new_row["predicted_ctr"] = predicted_ctr
-    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+    new_row["timestamp"] = datetime.now().isoformat()
+
+    # Ensure column order
+    new_row_ordered = {
+        "timestamp": new_row.get("timestamp"),
+        "hour": new_row.get("hour"),
+        "banner_pos": new_row.get("banner_pos"),
+        "device_type": new_row.get("device_type"),
+        "device_model": new_row.get("device_model"),
+        "predicted_ctr": new_row.get("predicted_ctr")
+    }
+
+    df = pd.concat([df, pd.DataFrame([new_row_ordered])], ignore_index=True)
     df.to_csv(LOG_PATH, index=False)
     return df
